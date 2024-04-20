ffbc9d2c0641b37ebc40512e322722e00285a9a4
Vignesh Gupta <vighneshgupta32@gmail.com>
2024-04-20 10:34:10 +0530

    feat(work-items): :pencil2: add data-table for work-items with dummy data

--- app/(main)/projects/[id]/work-items/page.tsx
+++ app/(main)/projects/[id]/work-items/page.tsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import { promises as fs } from "fs";
+import path from "path";
+import { z } from "zod";
 
-const WorkItemsPage = () => {
-  return (
-    <div>WorkItemsPage</div>
-  )
+import { columns } from "@/components/work-items/column";
+import { DataTable } from "@/components/work-items/data-table";
+import { taskSchema } from "@/components/work-items/data/schema";
+import { UserNav } from "@/components/work-items/user-nav";
+
+async function getTasks() {
+  const data = await fs.readFile(
+    path.join(process.cwd(), "components/work-items/data/tasks.json")
+  );
+
+  const tasks = JSON.parse(data.toString());
+
+  return z.array(taskSchema).parse(tasks);
 }
 
-export default WorkItemsPage
+const WorkItemsPage = async () => {
+  const tasks = await getTasks();
+
+  return (
+    <div className="hidden h-full flex-1 flex-col space-y-8 p-8 md:flex">
+      <DataTable data={tasks} columns={columns} />
+    </div>
+  );
+};
+
+export default WorkItemsPage;
---
+++ components/work-items/data/data.tsx
@@ -0,0 +1,71 @@
+import {
+  ArrowDownIcon,
+  ArrowRightIcon,
+  ArrowUpIcon,
+  CheckCircle,
+  CircleIcon,
+  CircleX,
+  CircleHelp,
+  Timer,
+} from "lucide-react";
+
+export const labels = [
+  {
+    value: "bug",
+    label: "Bug",
+  },
+  {
+    value: "feature",
+    label: "Feature",
+  },
+  {
+    value: "documentation",
+    label: "Documentation",
+  },
+];
+
+export const statuses = [
+  {
+    value: "backlog",
+    label: "Backlog",
+    icon: CircleHelp,
+  },
+  {
+    value: "todo",
+    label: "Todo",
+    icon: CircleIcon,
+  },
+  {
+    value: "in progress",
+    label: "In Progress",
+    icon: Timer,
+  },
+  {
+    value: "done",
+    label: "Done",
+    icon: CheckCircle,
+  },
+  {
+    value: "canceled",
+    label: "Canceled",
+    icon: CircleX,
+  },
+];
+
+export const priorities = [
+  {
+    label: "Low",
+    value: "low",
+    icon: ArrowDownIcon,
+  },
+  {
+    label: "Medium",
+    value: "medium",
+    icon: ArrowRightIcon,
+  },
+  {
+    label: "High",
+    value: "high",
+    icon: ArrowUpIcon,
+  },
+];
