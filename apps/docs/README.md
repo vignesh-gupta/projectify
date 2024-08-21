c904f0095c752a65bcb6e40a70a4eb2d4eb9ba01
Vignesh Gupta <vighneshgupta32@gmail.com>
2024-08-21 10:04:55 +0530

    remove all files for rollback

--- apps/docs/README.md
+++
@@ -1,5 +0,0 @@
-# Projectify Docs
-
-## DB Schema ER Diagram
-
-![DB Schema ER Diagram](https://raw.githubusercontent.com/vignesh-gupta/projectify/master/docs/assets/er-diagram.svg)
--- packages/backend/convex/auth.config.ts
+++
@@ -1,9 +0,0 @@
-// eslint-disable-next-line import/no-anonymous-default-export
-export default {
-  providers: [
-    {
-      domain: process.env.NEXT_PUBLIC_CLERK_ISSUER_BASE_URL,
-      applicationID: "convex",
-    },
-  ],
-};
