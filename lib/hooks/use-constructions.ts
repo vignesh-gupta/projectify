aa13dbded991a40a4edb011cae8a16e184dda632
Vignesh Gupta <vighneshgupta32@gmail.com>
2024-08-21 11:31:27 +0530

    remove all files for a empty project

--- lib/hooks/use-constructions.ts
+++
@@ -1,8 +0,0 @@
-import { useEffect } from "react";
-import { toast } from "sonner";
-
-export const useConstructions = (text: "page" | "area" | "feature") => {
-  return useEffect(() => {
-    toast.warning(`🚧 This ${text} is under construction`);
-  }, [text]);
-};
